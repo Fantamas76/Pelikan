89ad353eeb2d6c3cdb4e4d5337102a90502e402e
Fantamas76 <fantamas76@gmail.com>
2025-05-25 16:51:41 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01_view04-topaz-enhance-4x",
+      "name": "01_View04-topaz-enhance-4x",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1804006528134092,
+          "pitch": 0.33274593195539914,
+          "rotation": 4.71238898038469,
+          "target": "1-01_view05-topaz-enhance-4x"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01_view05-topaz-enhance-4x",
+      "name": "01_View05-topaz-enhance-4x",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -2.6215551058617965,
+        "pitch": 0.3071258489703599,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0153042933047196,
+          "pitch": 0.45505551652212084,
+          "rotation": 1.5707963267948966,
+          "target": "0-01_view04-topaz-enhance-4x"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
